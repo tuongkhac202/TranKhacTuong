3744173b9c148089236049e92f6ec9996ea43f8d
tuongkhac202 <87599130+tuongkhac202@users.noreply.github.com>
2024-05-15 17:42:30 +0700

    Update UI

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -39,3 +39,42 @@ function downloadCV() {
     link.download = "CV-Trần-Khắc-Tường.pdf";
     link.click();
 }
+
+// SLICK
+$(document).ready(function () {
+	$('.my-skills').slick({
+		infinite: true,
+		speed: 1500,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+		dots: true,
+		autoplay: true,
+		arrows: false,
+		autoplaySpeed: 4000,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 5,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 750,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+			}
+		]
+	});
+});
